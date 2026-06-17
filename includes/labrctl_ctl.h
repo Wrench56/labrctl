16be43e51c3b4ee086f7b0ae0a0bad317e6d7d0d
Mark Devenyi <markdevenyidev@gmail.com>
2026-06-17 00:07:15 -0400

    feat(xdp): add XDP UDP command filter

--- includes/labrctl_ctl.h
+++ includes/labrctl_ctl.h
@@ -1,19 +1,35 @@
 #ifndef LABRCTL_CTL_H
 #define LABRCTL_CTL_H
 
+#define LABRCTL_PORT 19552
+
 #ifdef __KERNEL__
 #include <linux/types.h>
 #else
 #include <stdint.h>
 typedef uint8_t __u8;
+typedef uint16_t __u16;
 typedef uint64_t __u64;
 #endif
+
+#define LABRCTL_MAGIC 0x4C
+#define LABRCTL_VERSION 0x00
 
 enum labrctl_op {
     LABRCTL_OP_NOP = 0,
     LABRCTL_OP_RUN = 1,
     LABRCTL_OP_KILL = 2,
 };
+
+/* Should fit into a single cache-line counting ETH + IP + UDP header */
+struct labrctl_packet {
+    /* Has to be LABRCTL_MAGIC & LABRCTL_VERSION */
+    __u16 hdr;
+    __u8 op;
+    __u8 arg;
+    __u8 seq;
+    __u8 rsvd[13];
+} __attribute__((packed));
 
 struct labrctl_ctl {
     __u64 epoch;
@@ -23,6 +39,7 @@ struct labrctl_ctl {
     __u8 rsvd[53];
 } __attribute__((packed)) ;
 
-#define CTL_SZ sizeof(struct labrctl_packet)
+#define CTL_SZ sizeof(struct labrctl_ctl)
+#define PACKET_SZ sizeof(struct labrctl_packet)
 
 #endif // LABRCTL_CTL_H
--- src/xpd/xpdfwd.c
+++ src/xpd/xpdfwd.c
@@ -1,11 +1,84 @@
-#include <linux/bpf.h>
-#include <bpf/bpf_helpers.h>
+#include <linux/types.h>
 
-SEC("xdp")
+#include <bpf/bpf_endian.h>
+#include <bpf/bpf_helpers.h>
+#include <linux/bpf.h>
+#include <linux/if_ether.h>
+#include <linux/in.h>
+#include <linux/ip.h>
+#include <linux/udp.h>
+
+#include <stdatomic.h>
+
+#include "labrctl_ctl.h"
+
+#define ETHHLEN (sizeof(struct ethhdr))
+#define UDPHLEN (sizeof(struct udphdr))
+
+extern int bpf_labrctl_submit(void* data, size_t data__sz) __ksym;
+
+static _Atomic __u32 seq_seen = 0;
+
+SEC("xdpfwd")
 int xdp_fwd(struct xdp_md* ctx)
 {
-    (void) ctx;
-    return XDP_PASS;
+    void* packet = (void*) (long) ctx->data;
+    void* packet_end = (void*) (long) ctx->data_end;
+
+    /* Strict check, needs VLAN stripping and no IP options */
+    if (packet_end - packet != 60) {
+        return XDP_PASS;
+    }
+
+    struct ethhdr* eth = packet;
+    if ((void*) (eth + 1) > packet_end) {
+        return XDP_PASS;
+    }
+
+    if (eth->h_proto != bpf_htons(ETH_P_IP)) {
+        return XDP_PASS;
+    }
+
+    struct iphdr* iph = (struct iphdr*) (packet + ETHHLEN);
+    if ((void*) (iph + 1) > packet_end) {
+        return XDP_PASS;
+    }
+
+    if (iph->protocol != IPPROTO_UDP) {
+        return XDP_PASS;
+    }
+
+    __u16 iph_len = iph->ihl * 4;
+    struct udphdr* udp = (struct udphdr*) (packet + ETHHLEN + iph_len);
+    if ((void*) (udp + 1) > packet_end) {
+        return XDP_PASS;
+    }
+
+    if (udp->dest != bpf_htons(LABRCTL_PORT)) {
+        return XDP_PASS;
+    }
+
+    void* payload = (void*) (udp + 1);
+    if (payload + PACKET_SZ > packet_end) {
+        return XDP_PASS;
+    }
+
+    struct labrctl_packet* pkt = payload;
+    if (pkt->hdr != bpf_htons((LABRCTL_MAGIC << 8) | LABRCTL_VERSION)) {
+        return XDP_PASS;
+    }
+
+    __u32 expected = (__u8) (pkt->seq - 1);
+    if (atomic_compare_exchange_strong(&seq_seen, &expected, pkt->seq)) {
+        bpf_labrctl_submit(payload, PACKET_SZ);
+        return XDP_DROP;
+    }
+
+    if ((__u8) expected == pkt->seq) {
+        return XDP_DROP;
+    }
+
+    return XDP_DROP;
 }
 
-char _license[] SEC("license") = "MIT";
+char _license[] SEC("license") = "Dual MIT/GPL";
